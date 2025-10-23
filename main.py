a16b70023b25ddb47c965760e0c76a3f10691723
Ran-Mewo <43445785+Ran-Mewo@users.noreply.github.com>
2025-10-23 22:33:09 +1100

    Reverify every 3 days

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def get_random_key_by_tier(checker_name: str, tier: str):
     return _get_checker_or_404(checker_name).get_key(tier)
 
 @app.on_event("startup")
-@repeat_every(seconds=60 * 60 * 24, wait_first=False)
+@repeat_every(seconds=60 * 60 * 24 * 3, wait_first=False)
 def verify_all_keys_daily():
     for checker in key_checkers:
         for key in list(checker.keys.keys()):
